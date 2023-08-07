04ea6a69e70a3847811acdac5f3fb7297b5113f0
Mike Solomon <mike@meeshkan.com>
2023-08-07 08:12:48 +0300

    Changes push

--- src/FRP/Event.js
+++ src/FRP/Event.js
@@ -1,27 +1,57 @@
 export const fastForeachThunk = (as) => {
-    for (var i = 0, l = as.length; i < l; i++) {
-        as[i]();
-    }
-}
+  for (var i = 0, l = as.length; i < l; i++) {
+    as[i]();
+  }
+};
 
 export const fastForeachE = (as, f) => {
-    for (var i = 0, l = as.length; i < l; i++) {
-        f(as[i]);
+  for (var i = 0, l = as.length; i < l; i++) {
+    f(as[i]);
+  }
+};
+/////////////////////////
+/////////////////////////
+/////////////////////////
+/////////////////////////
+/////////////////////////
+/////////////////////////
+/////////////////////////
+/////////////////////////
+/////////////////////////
+/////////////////////////
+/////////////////////////
+/////////////////////////
+/////////////////////////
+
+export const objHack = () => {
+  return [new Map()];
+};
+
+export const insertObjHack = (k, v, o) => {
+  o[o.length - 1].set(k, v);
+};
+
+export const deleteObjHack = (k, o) => {
+  for (const m of o) {
+    if (m.delete(k)) {
+      return true;
     }
-}
+  }
+  return false;
+};
 
 export const fastForeachOhE = (o, f) => {
-  o.forEach((v) => {
-    f(v);
-  });
-};
-
-export const insertObjHack = (k,v,o) => {
-  o.set(k, v);
-};
-export const deleteObjHack = (k, m) => {
-  return m.delete(k);
-};
-export const objHack = () => {
-  return new Map();
+  const M = new Map();
+  const run = (i) => {
+    o.push(new Map());
+    o[i].forEach((v, k) => {
+      f(v);
+      if (o[i + 1].size) run(i + 1);
+      o[i + 1].clear();
+      M.set(k, v);
+    });
+  };
+  run(0);
+  o.length = 0;
+  o.push(M);
 };
